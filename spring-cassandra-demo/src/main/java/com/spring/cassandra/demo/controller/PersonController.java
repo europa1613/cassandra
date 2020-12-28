dbf94e623efb2fd0b996964d523bef5abbba089a
Arvind <europa1613@users.noreply.github.com>
2020-12-28 15:28:45 -0600

    Controller endpoints

--- spring-cassandra-demo/src/main/java/com/spring/cassandra/demo/controller/PersonController.java
+++ spring-cassandra-demo/src/main/java/com/spring/cassandra/demo/controller/PersonController.java
@@ -1,14 +1,42 @@
 package com.spring.cassandra.demo.controller;
 
+import com.spring.cassandra.demo.dto.Person;
+import com.spring.cassandra.demo.repository.PersonRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
 
+  @Autowired
+  private PersonRepository repository;
+
   @GetMapping("/ping")
   public String ping() {
     return "pong";
+  }
+
+  @GetMapping("/people")
+  public List<Person> people() {
+    return repository.findAll();
+  }
+
+  @GetMapping(value = "/people", params = "first")
+  public List<Person> peopleByFirstName(@RequestParam String first) {
+    return repository.findByKeyFirstName(first);
+  }
+
+  @GetMapping(value = "/people", params = "last")
+  public List<Person> peopleByLastName(@RequestParam String last) {
+    return repository.findByLastName(last);
+  }
+
+  @GetMapping(value = "/people", params = "first_name_query")
+  public List<Person> peopleByFirstNameQuery(@RequestParam(name = "first_name_query") String name) {
+    return repository.findAllQuery(name);
   }
 
 }
