350efe18978a93d2d4413da39a19a39775ebcfca
Arvind <europa1613@users.noreply.github.com>
2020-12-27 22:21:54 -0600

    Bare commit without test

---
+++ spring-cassandra-demo/src/test/java/com/spring/cassandra/demo/springcassandrademo/SpringCassandraDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cassandra.demo.springcassandrademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCassandraDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
