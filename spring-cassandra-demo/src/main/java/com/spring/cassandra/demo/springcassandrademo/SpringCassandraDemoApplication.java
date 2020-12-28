55b4fadfb322e5bb03e7785ae5971c808dc2039b
Arvind <europa1613@users.noreply.github.com>
2020-12-27 22:27:17 -0600

    Exclude Cassandra auto config classes

--- spring-cassandra-demo/src/main/java/com/spring/cassandra/demo/springcassandrademo/SpringCassandraDemoApplication.java
+++ spring-cassandra-demo/src/main/java/com/spring/cassandra/demo/springcassandrademo/SpringCassandraDemoApplication.java
@@ -1,9 +1,14 @@
 package com.spring.cassandra.demo.springcassandrademo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {CassandraAutoConfiguration.class,
+		CassandraDataAutoConfiguration.class})
 public class SpringCassandraDemoApplication {
 
 	public static void main(String[] args) {
